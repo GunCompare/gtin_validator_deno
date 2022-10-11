025cf45d815b5695d0e77a84b51c6894b683dd47
DoUWant2DevApp <93307121+DoUWant2DevApp@users.noreply.github.com>
2022-10-11 05:09:35 -0400

    change folder name to Funcs

--- mod.ts
+++ mod.ts
@@ -1,12 +1,12 @@
-import { isValidCheckDigitOnGTIN } from "./ExportsAndTests/isValidCheckDigitOnGTIN.ts"
-import { isValidStringLengthForGTIN } from "./ExportsAndTests/isValidStringLengthForGTIN.ts"
-import { removeGTINLeadingZerosToUpcOrGTIN13 } from "./ExportsAndTests/removeGTINLeadingZerosToUpcOrGTIN13.ts"
-import { isValidGTIN } from "./ExportsAndTests/isValidGTIN.ts"
-import { getFormatOfGTIN } from "./ExportsAndTests/getFormatOfGTIN.ts"
-import { getFormatOfMinifiedGTIN } from "./ExportsAndTests/getFormatOfMinifiedGTIN.ts"
-import { calculateCheckDigitForGTIN } from "./ExportsAndTests/calculateCheckDigitForGTIN.ts"
-import { generateRandomGTIN } from "./ExportsAndTests/generateRandomGTIN.ts"
-import { generateArrayOfRandomGTINs } from "./ExportsAndTests/generateArrayOfRandomGTINs.ts"
+import { isValidCheckDigitOnGTIN } from "./Funcs/isValidCheckDigitOnGTIN.ts"
+import { isValidStringLengthForGTIN } from "./Funcs/isValidStringLengthForGTIN.ts"
+import { removeGTINLeadingZerosToUpcOrGTIN13 } from "./Funcs/removeGTINLeadingZerosToUpcOrGTIN13.ts"
+import { isValidGTIN } from "./Funcs/isValidGTIN.ts"
+import { getFormatOfGTIN } from "./Funcs/getFormatOfGTIN.ts"
+import { getFormatOfMinifiedGTIN } from "./Funcs/getFormatOfMinifiedGTIN.ts"
+import { calculateCheckDigitForGTIN } from "./Funcs/calculateCheckDigitForGTIN.ts"
+import { generateRandomGTIN } from "./Funcs/generateRandomGTIN.ts"
+import { generateArrayOfRandomGTINs } from "./Funcs/generateArrayOfRandomGTINs.ts"
 
 export {
   isValidGTIN,
